58fe3eadb71b69d03aed20af194af8d1dd00eee4
Sam <samjameslaw@gmail.com>
2021-09-10 16:26:02 +0100

    Cleanup

--- ChatApp.Core.API/Controllers/UsersController.cs
+++ ChatApp.Core.API/Controllers/UsersController.cs
@@ -15,11 +15,6 @@ namespace ChatApp.Core.API.Controllers
     [Route("[controller]")]
     public class UsersController : ControllerBase
     {
-        private static readonly string[] Summaries = new[]
-        {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-        };
-
         private readonly ILogger<UsersController> _logger;
         private readonly IHubContext<ChatHub> _hubContext;
         private readonly IUserRepository _userRepository;
@@ -29,19 +24,6 @@ namespace ChatApp.Core.API.Controllers
             _logger = logger;
             _hubContext = hubContext;
             _userRepository = userRepository;
-        }
-
-        [HttpGet]
-        public IEnumerable<WeatherForecast> Get()
-        {
-            var rng = new Random();
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateTime.Now.AddDays(index),
-                TemperatureC = rng.Next(-20, 55),
-                Summary = Summaries[rng.Next(Summaries.Length)]
-            })
-            .ToArray();
         }
 
         [HttpGet("all")]
